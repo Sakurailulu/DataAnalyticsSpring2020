b7d61a3ae015a98ac1a07cfdfe94a7a6837ae8c4
Sakurailulu <34151383+Sakurailulu@users.noreply.github.com>
2020-01-30 13:50:03 -0500

    Add files via upload

--- lab1_practice1.R
+++ lab1_practice1.R
@@ -5,3 +5,42 @@ fix(data1)
 tf<- is.na(EPI)
 E<- EPI[!tf]
 summary(E)
+
+fivenum(E.narm=TRUE)
+stem(EPI)
+hist(EPI,seq(30.,95.,1.0),prob=TRUE)
+line(density(EPI.na.rm=TRUE,bw=1.))
+rug(EPI)
+
+plot(ecdf(EPI), do.points=FALSE, verticals=TRUE) 
+par(pty="s") 
+qqnorm(EPI); qqline(EPI)
+x<-seq(30,95,1)
+qqplot(qt(ppoints(250), df = 5), x, xlab = "Q-Q plot for t dsn")
+qqline(x)
+boxplot(EPI,DALY) 
+
+
+EPI_South_Asia <- EPI[EPI_regions=='South Asia']
+tf<- is.na(EPI)
+E<- EPI[!tf]
+summary(E)
+
+fivenum(E.narm=TRUE)
+stem(EPI)
+hist(EPI,seq(30.,95.,1.0),prob=TRUE)
+line(density(EPI.na.rm=TRUE,bw=1.))
+rug(EPI)
+
+plot(ecdf(EPI), do.points=FALSE, verticals=TRUE) 
+par(pty="s") 
+qqnorm(EPI); qqline(EPI)
+x<-seq(30,95,1)
+qqplot(qt(ppoints(250), df = 5), x, xlab = "Q-Q plot for t dsn")
+qqline(x)
+boxplot(EPI,DALY) 
+
+
+
+
+
