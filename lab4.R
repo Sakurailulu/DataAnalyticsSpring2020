2ac222aedc5bdd30a8adfe5d81f7181164a7df28
Sakurailulu <34151383+Sakurailulu@users.noreply.github.com>
2020-03-05 15:26:29 -0500

    Create lab4.R
    in class

---
+++ lab4.R
@@ -0,0 +1,20 @@
+# creating a matrix data with random numbers
+# and plotting the matrix using the image() function
+# you will see there, it does not have a real pattern in the plot.
+set.seed(12345)
+help(par)
+# par can be used to set or query graphical parameters.
+# Parameters can be set by specifying them as arguments 
+# to par in tag = value form, or by passing them as a list of tagged values.
+par(mar = rep(0.2,4))
+data_Matrix <-matrix(rnorm(400), nrow = 40)
+image(1:10, 1:40, t(data_Matrix)[,nrow(data_Matrix):1])
+# now we can run a hierarchical cluster analysis on the dataset 
+# we will use the heatmap() function that is available in R
+help("heatmap") # read the documentation for # the heatmap() function that is available in #RStudio 
+#Read the documentation for rep()
+ help(rep)
+par(mar=rep(0.2,4))
+heatmap(data_Matrix)
+# When we run the heatmap() here, we get the dendrograms printed on the both columns and the rows and still there is no real immerging pattern that is interesting to us, 
+#it is because there is no real interesting pattern underlying in the data we generated.
